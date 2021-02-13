d8bae3139720e02db40bd2ccd5247c6edb6968bf
Taniket Mehra <taniket.mehra@joshtechnologygroup.com>
2021-02-13 16:11:03 +0530

    added list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import CardList from './Components/card-list/card-list.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			monsters: [],
+			searchField: '',
+		};
+	}
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then((res) => res.json())
+			.then((res) => this.setState(() => ({ ...this.state, monsters: res })));
+	}
+
+	render() {
+		const { monsters, searchField } = this.state;
+		const filteredMonsters = monsters.filter((monster) =>
+			monster.name.toLowerCase().includes(searchField.toLowerCase())
+		);
+
+		return (
+			<div className="App">
+				<input
+					type="search"
+					placeholder="search monsters"
+					value={searchField}
+					onChange={(e) => {
+						this.setState(() => ({
+							...this.state,
+							searchField: e.target.value,
+						}));
+					}}
+				/>
+				<CardList monsters={filteredMonsters} />
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/Components/card/card.component.js
@@ -0,0 +1,13 @@
+import './card.styles.css';
+
+const Card = (props) => {
+  return (
+    <div className='card-container'>
+      <img alt='monster' src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} />
+      <h2>{props.monster.name}</h2>
+      <h2>{props.monster.email}</h2>
+    </div>
+  );
+}
+
+export default Card;
